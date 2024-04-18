01d5ce03067f20765318e5802b2264d763cbe907
Al-Amin <alaminkhanshakil@gmail.com>
2024-04-18 19:51:52 +0600

    Create button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import Button from './components/Button';
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: 'Clark',
+    image: 'https://i.pravatar.cc/48?u=118836',
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: 'Sarah',
+    image: 'https://i.pravatar.cc/48?u=933372',
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: 'Anthony',
+    image: 'https://i.pravatar.cc/48?u=499476',
+    balance: 0,
+  },
+];
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+
+        <FormAddFriend />
+
+        <Button>Add friend</Button>
+      </div>
+
+      <FormSplitBill />
     </div>
   );
 }
