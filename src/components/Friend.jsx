53904837b6c8e36844c7432da7d3e0700b50216e
Al-Amin <alaminkhanshakil@gmail.com>
2024-04-19 21:25:24 +0600

    Create friend and friend list

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,22 +1,45 @@
-import PropTypes from 'prop-types';
-import Button from './Button';
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Friend = ({ friend }) => (
-  <li>
-    {friend.name}
-    <Button>Select</Button>
+const Friend = ({ friend, onSelection, selectedFriend }) => (
+  <li className={isSelected ? "selected" : ""}>
+    <img src={friend.image} alt={friend.name} />
+    <h3>{friend.name}</h3>
+
+    {friend.balance < 9 && (
+      <p className="red">
+        You owe {friend.name} {Math.abs(friend.balance)}$
+      </p>
+    )}
+
+    {friend.balance > 0 && (
+      <p className="green">
+        {friend.name} owes you
+        {Math.abs(friend.balance)}$
+      </p>
+    )}
+
+    {friend.balance === 0 && (
+      <p>
+        You and
+        {friend.name} are even
+      </p>
+    )}
+
+    <Button onClick={() => onSelection(friend)}>
+      {isSelected ? "Close" : "Select"}
+    </Button>
   </li>
 );
 
 Friend.propTypes = {
-  friend: PropTypes.shape(
-    {
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      image: PropTypes.string.isRequired,
-      balance: PropTypes.number.isRequired,
-    },
-  ).isRequired,
+  friend: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    balance: PropTypes.number.isRequired,
+  }).isRequired,
+  onSelection: PropTypes.func.isRequired,
 };
 
 export default Friend;
--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import Friend from './Friend';
 
-const FriendsList = ({ friends }) => (
+const FriendsList = ({ friends, onSelection, selectedFriend }) => (
   <ul>
     {friends.map((friend) => (
       <Friend
         friend={friend}
         key={friend.id}
+        selectedFriend={selectedFriend}
+        onSelection={onSelection}
       />
     ))}
   </ul>
@@ -21,6 +23,13 @@ FriendsList.propTypes = {
       balance: PropTypes.number.isRequired,
     }),
   ).isRequired,
+  onSelection: PropTypes.func.isRequired,
+  selectedFriend: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    balance: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default FriendsList;
